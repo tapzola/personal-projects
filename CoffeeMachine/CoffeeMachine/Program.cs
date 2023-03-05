a21d2f5062cb5f0181b3986d80b10f91e57d8836
tapzola <tapelo.dbe@gmail.com>
2023-03-04 22:24:33 -0700

    Coffee Machine

---
+++ CoffeeMachine/CoffeeMachine/Program.cs
@@ -0,0 +1,174 @@
+﻿namespace CoffeeMachine
+{
+    using System;
+    using System.Collections.Generic;
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var resources = new Dictionary<string, int>()
+            {
+                { "water", 300 },
+                { "milk", 200 },
+                { "coffee", 100 }
+            };
+
+            var menu = new Dictionary<string, Dictionary<string, object>>()
+            {
+                {
+                    "espresso", new Dictionary<string, object>()
+                    {
+                        { "ingredients", new Dictionary<string, int>()
+                        {
+                            { "water", 50 },
+                            { "coffee", 18 }
+                        }
+                        },
+                        { "cost", 1.5 }
+                    }
+                },
+                {
+                    "latte", new Dictionary<string, object>()
+                    {
+                        { "ingredients", new Dictionary<string, int>()
+                        {
+                            { "water", 200 },
+                            { "milk", 150 },
+                            { "coffee", 24 }
+                        }
+                        },
+                        { "cost", 2.5 }
+                    }
+                },
+                {
+                    "cappuccino", new Dictionary<string, object>()
+                    {
+                        { "ingredients", new Dictionary<string, int>()
+                        {
+                            { "water", 250 },
+                            { "milk", 100 },
+                            { "coffee", 24 }
+                        }
+                        },
+                        { "cost", 3.0 }
+                    }
+                }
+            };
+
+            var sadReply = "Sorry that's not enough money. Money refunded.";
+            var notEnoughReply = "Sorry there is not enough resources to make your coffee.";
+
+            Func<Dictionary<string, int>, string, string> CheckResources = (currentResources, preference) =>
+            {
+                int water = 0;
+                int coffee = 0;
+                int milk = 0;
+             
+
+
+                if (preference == "espresso")
+                {
+                    var ingredients = (Dictionary<string, int>)menu[preference]["ingredients"];
+                    water = currentResources["water"] - ingredients["water"];
+                    coffee = currentResources["coffee"] - ingredients["coffee"];
+                    milk = currentResources["milk"];
+
+                }
+                else if (preference == "latte" || preference == "cappuccino")
+                {
+                    var ingredients = (Dictionary<string, int>)menu[preference]["ingredients"];
+                    water = currentResources["water"] - ingredients["water"];
+                    coffee = currentResources["coffee"] - ingredients["coffee"];
+                    milk = currentResources["milk"] - ingredients["milk"];
+
+                }
+
+                if (water < 0 || coffee < 0 || milk < 0)
+                {
+                    return notEnoughReply;
+                }
+                else
+                {
+                    currentResources["water"] = water;
+                    currentResources["coffee"] = coffee;
+                    currentResources["milk"] = milk;
+                    return "Please insert coins";
+                }
+            };
+
+            Func<string, string> ProcessCoins = (preference) =>
+            {
+                Console.Write("How many quarters: ");
+                var q1 = Console.ReadLine();
+                double quarters = Convert.ToDouble(q1);
+                Console.Write("How many dimes: ");
+                var q2 = Console.ReadLine();
+                double dimes = Convert.ToDouble(q2);
+                Console.Write("How many nickles: ");
+                var q3 = Console.ReadLine();
+                double nickles = Convert.ToDouble(q3);
+                Console.Write("How many pennies: ");
+                var q4 = Console.ReadLine();
+                double pennies = Convert.ToDouble(q4);
+
+                double total = quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01;
+                var totalMoney = total - (double)menu[preference]["cost"];
+                if (totalMoney < 0)
+                {
+                    return sadReply;
+                }
+                else
+                {
+                    return $"Here is {totalMoney} in change\n Here is your {preference}. Enjoy!";
+                }
+            };
+            int count = 0;
+            bool isPlaying = true;
+            while (isPlaying)
+            {
+                //Dictionary<string, int> currentResources = new Dictionary<string, int>(resources);
+                Console.Write("What would you like? (espresso/latte/cappuccino):\n");
+                var userPreference = Console.ReadLine();
+
+                if (userPreference == "off")
+                {
+                    isPlaying = false;
+                }
+                else if (userPreference == "report" && count == 0)
+                {
+                    foreach (KeyValuePair<string, int> entry in resources)
+                    {
+                        Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+                    }
+                }
+                else if (userPreference == "report" && count >= 1)
+                {
+                    foreach (KeyValuePair<string, int> entry in resources)
+                    {
+                        Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+                    }
+                }
+                else
+                {
+                    var specificIngredients = menu[userPreference]["ingredients"];
+                    count++;
+                    string check = CheckResources(resources, userPreference);
+                    Console.WriteLine(check);
+                    if (check == notEnoughReply)
+                    {
+                        isPlaying = false;
+                    }
+                    else
+                    {
+                        string coins = ProcessCoins(userPreference);
+                        Console.WriteLine(coins);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ CoffeeMachine/README.md
@@ -0,0 +1,24 @@
+# Overview
+
+I created a virtual  coffee machine program that is coin operated, can make 3 flavors, the coffee machine has some resources that it has to manage, it cannot make more coffee if the resources are not enough, it will check if the amount of coins put by the user is sufficient for the price of that specific coffee or not, it will deduct total resources available from the resources that make the specific type of coffee. 
+
+I wrote this software to demonstrate my competency in understanding the C# language. 
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used Visual Studio as my IDE to write this code. I used C# language to create this software.
+s a modern, object-oriented, and type-safe programming language. C# enables developers to build many types of secure and robust applications that run in .NET. C# has its roots in the C family of languages and it is familiar to C, C++, Java, and JavaScript languages. 
+
+# Useful Websites
+
+
+- [W3schools](https://www.w3schools.com/cs/index.php)
+- [YouTube Video](https://youtu.be/GhQdlIFylQ8)
+
+# Future Work
+
+- Add emojis to my virtual machine so it looks better
+- Ask the user the question again even if the resources are not enough instead of ending the code
